560fb035501df02f58aac3499168a5a73dc6b6a9
Rishabh068 <anshhhu@gmail.com>
2020-07-20 11:29:35 +0530

    new things added

--- lib/home.dart
+++ lib/home.dart
@@ -3,68 +3,103 @@ import 'package:kidsgame/colors.dart';
 import 'package:kidsgame/colmatch.dart';
 import 'package:kidsgame/symbols.dart';
 import 'package:kidsgame/wordmatch.dart';
+import 'package:lottie/lottie.dart';
 
 class Home extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    double height= MediaQuery.of(context).size.height ;
-    height= height - 90;
+    double height = MediaQuery.of(context).size.height;
+    height = height - 90;
     return Scaffold(
-      appBar: AppBar(title: Text("Home"),centerTitle: true,),
-      body: SingleChildScrollView(child:
-        Column(children: [
-
-            Row(
+        appBar: AppBar(
+          title: Text("Home"),
+          centerTitle: true,
+        ),
+        body: SingleChildScrollView(
+          child: Column(
             children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(height: height/2,
-                  decoration: BoxDecoration(border: Border.all(color: Colors.teal)),
-                  child: FlatButton(child: Text("Colour Match",style: TextStyle(fontSize: 20),),onPressed: ()=>Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>ColorGame())),),
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: height / 2,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.teal),
+                        ),
+                        child: FlatButton(
+                          child: Lottie.asset(
+                              'assets/25646-numag-color-copy.json'),
+                          onPressed: () => Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => ColorGame())),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: height / 2,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.teal),
+                        ),
+                        child: FlatButton(
+                          child: Lottie.asset('assets/25578-letter-l.json'),
+                          onPressed: () => Navigator.pushReplacement(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Wordgame())),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(height: height/2,
-                    decoration: BoxDecoration(border: Border.all(color: Colors.teal)),
-                    child: FlatButton(child: Text("Word Match",style: TextStyle(fontSize: 20),),onPressed: ()=>Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Wordgame())),),
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: height / 2,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.teal),
+                        ),
+                        child: FlatButton(
+                          child: Lottie.asset('assets/4659-avocad-bros.json'),
+                          onPressed: () => Navigator.pushReplacement(context,
+                              MaterialPageRoute(builder: (context) => Game())),
+                        ),
+                      ),
+                    ),
                   ),
-                ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        height: height / 2,
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.teal),
+                        ),
+                        child: FlatButton(
+                          child:
+                              Lottie.asset('assets/25172-pencil-loader.json'),
+                          onPressed: () => Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(builder: (context) => Symbols()),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               )
             ],
           ),
-
-
-             Row(
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(height: height/2,
-                      decoration: BoxDecoration(border: Border.all(color: Colors.teal)),
-                      child: FlatButton(child: Text("Vegetable Match",style: TextStyle(fontSize: 20),),onPressed: ()=>Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Game())),),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(height: height/2,
-                      decoration: BoxDecoration(border: Border.all(color: Colors.teal)),
-                      child: FlatButton(child: Text("Symbols Match",style: TextStyle(fontSize: 20),),onPressed: ()=>Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Symbols()),),
-                    ),
-                  ),
-                ),
-                )
-              ],
-            )
-
-
-        ],),)
-    );
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,42 +3,44 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kidsgame/home.dart';
 
+import 'package:lottie/lottie.dart';
+
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Splash(),
     );
   }
 }
+
 class Splash extends StatefulWidget {
   @override
   _SplashState createState() => _SplashState();
 }
 
 class _SplashState extends State<Splash> {
-
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-   Timer(Duration(seconds: 1),()=>Navigator.pushReplacement(context,new MaterialPageRoute(builder: (context)=>Home())));
+
+    Future.delayed(
+        Duration(seconds: 5),
+        () => Navigator.pushReplacement(
+            context, new MaterialPageRoute(builder: (context) => Home())));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(Icons.thumb_up,size: 40,)
-        ],
-      ),
-    );
+    return Scaffold(
+        body: Container(
+      margin: EdgeInsets.only(top: 70),
+      child: Lottie.asset('assets/27451-lock-action.json'),
+    ));
   }
 }
